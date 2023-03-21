c4cc6986cb2c92ec025388e9a1cf13ae5a7423bc
Silva <john.silva@capgemini.com>
2023-03-21 17:52:07 -0300

    Module 2 - Events: fetch

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ console.log('Progressive Enhancement: (SW Supported)', navigator);
 
 // if ('serviceWorker' in navigator)
 if (navigator.serviceWorker) {
-    console.log('service worker supported!');
+    console.log('service worker supported!', navigator.serviceWorker);
     navigator.serviceWorker
         .register('./sw.js')
         .then(function(registration) {
             console.log('Then Registered', registration);
         });
 }
+
+// Get camera feed
+fetch('camera_feed.html')
+    .then((res) => {
+        resBody = res.text();
+        console.log('fetch', resBody);
+        return resBody;
+    })
+    .then((html) => {
+        console.log('fetch html', html);
+
+        document.getElementById('camera').innerHTML = html;
+    })
--- sw.js
+++ sw.js
@@ -1,24 +1,46 @@
 console.log('SW: registered');
 
-self.addEventListener('install', (e) => {
-    console.log('SW: install Event');
+// self.addEventListener('install', (e) => {
+//     console.log('SW: install Event');
 
-    // let installPromise = new Promise((resolve) => {
-    //     setTimeout(resolve, 3000);
-    // });
+//     // let installPromise = new Promise((resolve) => {
+//     //     setTimeout(resolve, 3000);
+//     // });
 
-    // e.waitUntil(installPromise);
+//     // e.waitUntil(installPromise);
 
-    // This will force the installation of the new service worker over the current one
-    self.skipWaiting();
-});
+//     // This will force the installation of the new service worker over the current one
+//     self.skipWaiting();
+// });
 
-self.addEventListener('activate', (e) => {
-    console.log('SW: Activate Event Bola');
+// self.addEventListener('activate', (e) => {
+//     console.log('SW: Activate Event Bola');
 
-    let activatePromise = new Promise((resolve) => {
-        setTimeout(resolve, 3000);
-    });
+//     let activatePromise = new Promise((resolve) => {
+//         setTimeout(resolve, 3000);
+//     });
 
-    e.waitUntil(activatePromise);
+//     e.waitUntil(activatePromise);
+// });
+
+self.addEventListener('fetch', (ev) => {
+    if (ev.request.url.endsWith('style.css')) {
+        console.log('style.css');
+        console.log('ev.request.url', ev.request.url);
+
+        ev.respondWith( fetch('/style2.css'));
+    }
+
+    if (ev.request.url.endsWith('/camera_feed.html')) {
+        ev.respondWith(
+            fetch(ev.request)
+                .then((res) => {
+                    console.log('res.ok', res.ok);
+
+                    if (res.ok) return res;
+
+                    return new Response('Camera feed not available!');
+                })
+        )
+    }
 })
