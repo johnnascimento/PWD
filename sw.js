4c2199a58cb2fbd796ac6da5aae100c05e89999a
Silva <john.silva@capgemini.com>
2023-03-22 17:01:09 -0300

    Module 2 - ServiceWorkerResgitration

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
 console.log('SW: registered');
 
+// START - Module 2 - ServiceWorkerRegistration
+
+self.addEventListener('install', (ev) => {
+    console.log('Module 2 - ServiceWorkerRegistration class');
+    console.log('install 5');
+    console.log('SW - ev', ev);
+
+    ev.waitUntil(new Promise((resolve) => setTimeout(resolve, 5000)));
+});
+
+self.addEventListener('activate', (ev) => {
+    console.log('Module 2 - ServiceWorkerRegistration class');
+    console.log('activate 5');
+    console.log('SW - ev', ev);
+});
+
+// END - Module 2 - ServiceWorkerRegistration
+
 // self.addEventListener('install', (e) => {
 //     console.log('SW: install Event');
 
@@ -23,24 +41,24 @@ console.log('SW: registered');
 //     e.waitUntil(activatePromise);
 // });
 
-self.addEventListener('fetch', (ev) => {
-    if (ev.request.url.endsWith('style.css')) {
-        console.log('style.css');
-        console.log('ev.request.url', ev.request.url);
+// self.addEventListener('fetch', (ev) => {
+//     if (ev.request.url.endsWith('style.css')) {
+//         console.log('style.css');
+//         console.log('ev.request.url', ev.request.url);
 
-        ev.respondWith( fetch('/style2.css'));
-    }
+//         ev.respondWith( fetch('/style2.css'));
+//     }
 
-    if (ev.request.url.endsWith('/camera_feed.html')) {
-        ev.respondWith(
-            fetch(ev.request)
-                .then((res) => {
-                    console.log('res.ok', res.ok);
+//     if (ev.request.url.endsWith('/camera_feed.html')) {
+//         ev.respondWith(
+//             fetch(ev.request)
+//                 .then((res) => {
+//                     console.log('res.ok', res.ok);
 
-                    if (res.ok) return res;
+//                     if (res.ok) return res;
 
-                    return new Response('Camera feed not available!');
-                })
-        )
-    }
-})
+//                     return new Response('Camera feed not available!');
+//                 })
+//         )
+//     }
+// })
